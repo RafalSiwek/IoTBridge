19dcecefce5e22ae54943304ba822f121cab6a6f
RafalSiwek <rafal.siwek@o2.pl>
2021-03-23 16:54:26 +0100

    Added device handling feature

--- balluff_master_connector/code/app.py
+++ balluff_master_connector/code/app.py
@@ -3,28 +3,38 @@ import logging
 from flask_mqtt import Mqtt
 import json
 import datetime
+import os
+
+device_ip=os.environ.get('MASTER_IP')
+if device_ip is None:
+    raise SystemExit("Setup MASTER_IP envirnoment variable ...")
+
 
 app = Flask(__name__)
-app.config['MQTT_CLIENT_ID'] = 'aws_handler_mqtt'
+app.config['MQTT_CLIENT_ID'] = device_ip
 app.config['MQTT_CLEAN_SESSION'] = True
 app.config['MQTT_BROKER_URL'] = 'mqtt_broker'
 app.config['MQTT_BROKER_PORT'] = 1883
 app.config['MQTT_REFRESH_TIME'] = 0.2  # refresh time in seconds
 mqtt = Mqtt(app)
-mqtt.subscribe("#")
-#mqtt.loop_start()
+global run
+run = False
 
+@app.route('/start')
+def start():
+    run = True
+    return 'Start IoT connector for device IP '+device_ip
 
-
-@app.route('/hi')
-def hi():
-    logging.debug("Invoking HI...")
-    mqtt.publish("hi",1)
-    return 'Hey, I am doing some crazy-ass web-service shit!'
+@app.route('/stop')
+def stop():
+    run = False
+    return 'Stop IoT connector for device IP '+device_ip
 
 @mqtt.on_log()
 def handle_logging(client, userdata, level, buf):
     print(level, buf)
+
+
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
